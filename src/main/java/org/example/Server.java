3735d5a3157c83d7287e365b20b7a45603cb7f1e
Noah Jutz <noah@noahjutz.com>
2025-12-13 13:17:47 +0100

    Implement basic HTTP Server

---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import jakarta.ws.rs.ext.RuntimeDelegate;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Server {
+  static void main() throws IOException {
+    final var config = new ResourceConfig();
+    final var server = HttpServer.create(new InetSocketAddress(8080), 0);
+    final var handler = RuntimeDelegate.getInstance().createEndpoint(config, HttpHandler.class);
+    server.createContext("/api/v1", handler);
+    server.start();
+  }
+}
