0d8d306c3b08ae5f3d7bce19fa14a1953dde52f5
Noah Jutz <noah@noahjutz.com>
2025-12-13 17:00:40 +0100

    Implement endpoint for checking List<Point> for distance

--- src/main/java/org/example/PointCloudService.java
+++ src/main/java/org/example/PointCloudService.java
@@ -1,13 +1,36 @@
 package org.example;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import org.example.entity.Point;
 
-@Path("point-clouds")
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+@Path("points")
 public class PointCloudService {
   @GET
   @Path("health")
   public String health() {
     return "Hello World!";
+  }
+
+  @POST
+  public Optional<Double> processPointCloud(List<Point> points) {
+    final var distance =
+        points.stream()
+            .map(p -> distance(p.getX(), p.getY(), p.getZ()))
+            .min(Comparator.naturalOrder());
+
+    if (distance.isPresent() && distance.get() <= 10) {
+      return distance;
+    }
+    return Optional.empty();
+  }
+
+  private double distance(double x, double y, double z) {
+    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
   }
 }
---
+++ src/main/java/org/example/entity/Point.java
@@ -0,0 +1,10 @@
+package org.example.entity;
+
+import lombok.Data;
+
+@Data
+public class Point {
+    private double x;
+    private double y;
+    private double z;
+}
